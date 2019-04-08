728dac4fca87a89caa77d031a9cf02c1e44f6c38
kkrishnan90 <k.krishnan.90@gmail.com>
2019-04-08 19:13:50 +0530

    update config.json

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    api_server: 'http://129.213.168.18/',
+    api_server: 'http://129.213.190.43/',
     path_authenticate: 'users/authenticate',
     path_getLatestPosts: 'posts/feed/',
     path_resetPassword: 'users/resetPassword',
@@ -7,6 +7,8 @@ export const config = {
     path_createMessage: 'posts/create',
     path_validate_otp: 'users/validateOTP',
     path_generate_otp: 'users/generateOTP',
-    obj_store_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/p/ShyUyJr14FZAa4rMfwNRvAFgPYzhLWp4IULM_vTv-yQ/n/gse00014894/b/sc_symposium/o/",
-    obj_store_read_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/n/gse00014894/b/sc_symposium/o/",
+    //obj_store_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/p/ShyUyJr14FZAa4rMfwNRvAFgPYzhLWp4IULM_vTv-yQ/n/gse00014894/b/sc_symposium/o/",
+    obj_store_url:"https://objectstorage.us-ashburn-1.oraclecloud.com/p/YV5gJ1EFgCByGwW-RerZyv1weP1s7nmUsW5ikxNPRJE/n/krish30990/b/krish/o/sc_symposium_write",
+    //obj_store_read_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/n/gse00014894/b/sc_symposium/o/",
+    obj_store_read_url:"https://objectstorage.us-ashburn-1.oraclecloud.com/p/nJEY4sF4C1wtR4GW3Iaz8GOC6dxN5PToJRge2KtRkKs/n/krish30990/b/krish/o/sc_symposium"
 }
